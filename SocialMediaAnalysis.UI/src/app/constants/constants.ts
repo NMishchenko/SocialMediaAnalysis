219790ec78fbe27e0e3424db82b0624a3d23a06c
Volodymyr Riabenko <vova.riabenko@gmail.com>
2024-02-10 14:54:19 +0200

    add chart
    add

---
+++ SocialMediaAnalysis.UI/src/app/constants/constants.ts
@@ -0,0 +1,14 @@
+﻿export const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
